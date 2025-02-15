cc0a785436fe5da8ea2f4a910c30f306b405847f
Ana3Lu <anaquiva@unisabana.edu.co>
2025-02-15 17:59:48 -0500

    Conecta API interna con postgresql

--- src/test/java/co/edu/unisabana/internalapi/InternalapiApplicationTests.java
+++ src/test/java/co/edu/unisabana/internalapi/InternalapiApplicationTests.java
@@ -2,12 +2,14 @@ package co.edu.unisabana.internalapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-h2.properties")
 class InternalapiApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
+
